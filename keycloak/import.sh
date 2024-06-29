8dc588d3379ac9a34d3b43b58c5687a57344d9de
Martin Besozzi <embesozzi@gmail.com>
2024-06-28 21:42:29 -0300

    feat: Update Keycloak to v25.0.1 and OpenFGA platform to v1.5.5. Update custom extension

--- keycloak/import.sh
+++ keycloak/import.sh
@@ -30,4 +30,3 @@ echo "Creating PoC Users, Role Model, User Role Assigments and Clients"
 # User Role Assignments
 /opt/keycloak/bin/kcadm.sh add-roles -r master --uusername paula --rolename analyst-catalog
 /opt/keycloak/bin/kcadm.sh add-roles -r master --uusername richard --rolename admin-catalog
-
