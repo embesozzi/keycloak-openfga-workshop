e23cd41432a273ce0100e61648860aeeb1a643a2
Martin Besozzi <embesozzi@gmail.com>
2024-07-12 11:48:01 -0300

    feat: Update product image links

--- store-api/src/data.js
+++ store-api/src/data.js
@@ -5,7 +5,7 @@ module.exports = {
       name: 'Glasses Ray Ban',
       category: "pre-sale",
       description: 'Ray-Ban Black Original Wayfarer Classic Sunglasses',
-      url : "https://source.unsplash.com/K62u25Jk6vo/600x300",
+      url : "https://images.unsplash.com/photo-1418147396505-c57fac80c7f6",
       details : "Brand	<b>Ray-Ban</b> <br> Model Name	Stories <br> Style	Stories<br> Color	Shiny Blue/Dark Blue Polarized <br> Age Range (Description)	Adult",
       status : "publish"
     },
@@ -14,7 +14,7 @@ module.exports = {
       name: 'Apple watch',
       category: "pre-sale",
       description: 'Apple Watch Series 3 42MM Special Features',
-      url : "https://source.unsplash.com/2cFZ_FB08UM/600x300",
+      url : "https://images.unsplash.com/photo-1598516802414-50a01bee818d",
       details : "Brand	<b>Apple</b> <br> Model Name	Apple Watch Series <br> Style	GPS <br> Special Feature Activity Tracker, Heart Rate Monitor, Sleep Monitor, Blood Oxygen",
       status : "publish"
     },
@@ -23,7 +23,7 @@ module.exports = {
       name: 'Headphones Bose',
       category: "pre-sale",
       description: 'Bose Noise Cancelling Wireless Headphones 700',
-      url : "https://source.unsplash.com/vISNAATFXlE/600x300",
+      url : "https://images.unsplash.com/photo-1674658556545-f18d4080ab6c",
       details: "Brand	<b>Bose</b> <br> Audio Model Name	<br> Performance ANC HeadphonesColor<br>Technology	Bluetooth 5.0",
       status: "published"
     },
@@ -32,7 +32,7 @@ module.exports = {
       name: 'Nikon Camera',
       category: "pre-sale",
       description: "Nikon Camera Z50 Two Lens Coolpix B500",
-      url : "https://source.unsplash.com/dcgB3CgidlU/600x300",
+      url : "https://images.unsplash.com/photo-1460134846237-51c777df6111",
       status : "publish",
       details: "Brand <b>Nikon</b> <br>Model Name	Nikon Coolpix B500 <br>Form Factor	Point and Shoot <br>Effective Still Resolution	16 MP",
       status : "published"
@@ -42,7 +42,7 @@ module.exports = {
       name: 'Chanel N°5 Perfume',
       category: "pre-sale",
       description: 'Ulric De Varens Gold Issime Pour Elle 75ml Estilo Chanel Nº5.',
-      url : "https://source.unsplash.com/potCPE_Cw8A/600x300",
+      url : "https://images.unsplash.com/photo-1458538977777-0549b2370168",
       details : "Brand	<b>CHANEL<b> <br>Item Form	Spray <br>Item Volume	3.4 Fluid Ounces <br>  Age Range (Description)Adult",
       status : "published"
     }
